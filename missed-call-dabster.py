c38301ae90e637d1673e7cfda4fc4febde333c98
Gagan0123 <gagan@iamme.in>
2013-08-12 02:06:32 +0530

    Removed the absurd Copyright notice I had put on the file, its open source, not copyrighted

--- missed-call-dabster.py
+++ missed-call-dabster.py
@@ -1,11 +1,3 @@
-#Copyright 2010 Gagan Deep Singh(Quest World)
-#Licensed under the Apache License, Version 2.0 (the "License"); 
-#you may not use this file except in compliance with the License. 
-#You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 
-#Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License. 
-
-
-
 #To use this app, just press up arrow key to start your first call making(give the phone number and the number of times)
 #To force reset in case it starts giving errors, press down arrow key
 #Use the RED colored END key to terminate it in between the miss call process is going on
